c9c8ab86cdc0c40456ed19cc8af34d1a4108d71a
Dmytro Demianenko <83134601+DmytroDemianenko@users.noreply.github.com>
2021-11-11 16:41:43 +0100

    rt

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
-import './sass/main.scss';
+import * as _ from "lodash";
+import * as PNotify from "@pnotify/core";
+import * as PNotifyMobile from "@pnotify/mobile";
+import "@pnotify/core/dist/BrightTheme.css";
+import countryCard from './templates/country-cards.hbs';
+// import countryList from './templates/hbs/country-list.hbs';
+import './styles.css';
+import fetchCountries from './js/fetchCountries';
+
+  const refs = {
+    searchInput: document.querySelector('.js-input'),
+    articlesContainer: document.querySelector('.js-articles-container'),
+  };
+  fetchCountries();
+
+
+
+  // console.log(fetchCountries());
+  // function onSearch(e) {
+  //   e.preventDefault();
+  //   fetchCountry(e)
+  //     // .then(data => data.ok ? data : Promise.reject(data))
+  //     .then((data) => {
+  //       refs.countryCardContainer.innerHTML = "";
+  //       myStack.close(true);
+  //       if (data.length === 1) {
+  //         refs.countryCardContainer.insertAdjacentHTML("beforeend", countryCardTpl(data[0]))
+  //       } else if (data.length < 11) {
+  //         data.forEach((country) => {
+  //           console.log(country.name);
+  //         });
+  //         refs.countryCardContainer.insertAdjacentHTML("beforeend", countryList(data))
+  //       } else if (data.length > 10) {
+  //         PNotify.notice({
+  //           text: "Too many matches found. Please, enter a more specific name!",
+  //           stack: myStack,
+  //           modules: new Map([...PNotify.defaultModules, [PNotifyMobile, {}]]),
+  //         })
+  //       }
+  //     })
+  //     .catch(
+  //       PNotify.notice({
+  //       title: 'ERROR!',
+  //         text: "Such country doesn't exist!Try to type something normal O.o",
+  //         stack: myStack,
+  //         modules: new Map([...PNotify.defaultModules, [PNotifyMobile, {}]]),
+  //       })
+  //     )
+  // }
